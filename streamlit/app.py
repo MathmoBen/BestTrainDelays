4b063306daf8c25d18d8bc1b623c0707a538f515
YvngJoey <yvngjoey@JOKC1.localdomain>
2023-12-06 12:01:20 +0000

    app.py update

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import numpy as np
 import pickle
-from scripts.preprocessing.preprocess import preprocessing_pipe
+#from scripts.preprocessing.preprocess import preprocessing_pipe
 import datetime
 
 
@@ -13,77 +13,48 @@ st.set_page_config(page_title= "Ex-stream-ly Cool App",
      initial_sidebar_state="expanded"
     )
 
-model = pickle.load(open("/home/yvngjoey/code/MathmoBen/TrainDelays/Model/model.pkl", 'rb'))
-pipe = pickle.load(open("/home/yvngjoey/code/MathmoBen/TrainDelays/Model/pipe.pkl", 'rb'))
+# model = pickle.load(open("/home/yvngjoey/code/MathmoBen/TrainDelays/Model/model.pkl", 'rb'))
+# pipe = pickle.load(open("/home/yvngjoey/code/MathmoBen/TrainDelays/Model/pipe.pkl", 'rb'))
 
 
 st.markdown("""# Train Delay Estimator
 """)
 
 
-columns = st.columns(3)
-
-option1 = st.selectbox(
-     'Select your travel origin ',
-     ('Hoxton', 'Haggerston', 'Shoreditch Street', 'White chapel', \
-         'Wembley', 'Earls court', 'Liverpool street', 'Old Street', \
-             'Euston Square', 'Chancery lane', 'Holborn', 'Kings Cross St Pancras'))
-
-columns[0].option1
+df = pd.read_csv('/home/yvngjoey/code/MathmoBen/BestTrainDelays/streamlit/lookup_for_streamlit.csv')
+first_df = df[['Station Name']]
 
 
+origin = st.selectbox(
+     'Select your travel origin ', first_df)
 
-option2 = st.selectbox(
-    'Select your travel destination',
-    ('hoxton', 'haggerston', 'chapel'))
+origin_date = st.date_input('origin_date', datetime.date(2023, 6, 19))
 
-d = st.date_input('What day are you departing' ,
-              datetime.date(2023, 4, 6))
-
-t = st.time_input('Select your departing time',
-                  datetime.time(1,30))
+origin_time = st.time_input('origin_time', datetime.time(1, 30))
 
 
+destination = st.selectbox('Select your travel destination ', first_df)
 
-option3 = st.selectbox(
-    'What day are you leaving',
-    ('Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'))
+destination_date = st.date_input('destination_date', datetime.date(2023, 6, 19))
 
-
-
-option4 = st.selectbox(
-    'Select type of day',
-    ('SA', 'WD', 'SU', 'BH', 'BD'))
-
-st.write('SA - Saturday, SU - Sunday, WD - Weekday, BH - Bank Holiday,  BD - Boxing Day')
-
-# We take the features we have selected
-# We convert them from easy-to-understand strings into a format the model expects
-# e.g. We have a dataframe that maps the string 'Hoxton' to 0.324, 0.456456
-
-# We send them to the API
-# The API sends back a number (e.g 5)
-
-# We do st.write (f'{5} min wait)
+destination_time = st.time_input('destination_time', datetime.time(1, 30))
 
 
-option6 = st.selectbox(
-    'Pick a previously affected group code',
-    ('EK01', 'EK04', 'EK99', 'EK02', 'EK03', 'EK05'))
-
-
-option7 = st.selectbox(
+train_service_code = st.selectbox(
     'Pick a train service code',
     ('22214000', '22204000', '21921000', '25234001', '21234001', '21252001',
        '25235001', '22216000', '22206000', '21235001', '22218000', '22215003',
        '21237001', '22215002'))
 
+train_service_group_code = st.selectbox(
+    'Pick a previously affected group code',
+    ('EK01', 'EK02', 'EK03', 'EK04', 'EK05', 'EK99'))
 
-st.selectbox('Applicable-Timetable',
-             ('Y', 'N'))
+Applicable_Timetable = st.checkbox('Applicable-Timetable')
 
-st.selectbox('Previous train incident reason',
+Incident_reason = st.selectbox('Previous train incident reason',
              ('A', 'D', 'F', 'I', 'J','M', 'O', 'P', 'Q', 'R','T', 'V', 'X', 'Z'))
+
 
 st.write('A: Freight Terminal Operations Cause,\
         D : Holding codes, \
@@ -101,37 +72,58 @@ st.write('A: Freight Terminal Operations Cause,\
 )
 
 
-st.selectbox('Pick a previously affected train unit class',
+train_class_unit = st.selectbox('Pick a previously affected train unit class',
              ('313', '317', '315', '321','375', '378', '710'))
 
+event_code = st.checkbox('PERFORMANCE_EVENT_CODE')
 
-#st.write(f'{option1}, {option2}, {option3}, {option4}, {option6}, {option7}')
+
+
+
+
+
+
+
+
+
+
+# # We take the features we have selected
+# # We convert them from easy-to-understand strings into a format the model expects
+# # e.g. We have a dataframe that maps the string 'Hoxton' to 0.324, 0.456456
+
+# # We send them to the API
+# # The API sends back a number (e.g 5)
+
+# # We do st.write (f'{5} min wait)
+
+
+
+#
 
 # hoxton, hoxton, Mon, SA, EK01, 22214000
 # ENGLISH_DAY_TYPE, SERVICE_GROUP_CODE_AFFECTED, TRAIN_SERVICE_CODE_AFFECTED
 
 
-df1 = pd.read_csv('/home/yvngjoey/code/MathmoBen/TrainDelays/mapping_data/stanox-lookup-lc.csv')
-#st.dataframe(df1)
+# df1 = pd.read_csv('/home/yvngjoey/code/MathmoBen/TrainDelays/mapping_data/stanox-lookup-lc.csv')
+# #st.dataframe(df1)
 
-total_df = pd.read_csv('/home/yvngjoey/code/MathmoBen/TrainDelays/Notebooks/clean_data_final.csv')
-#st.write(total_df.columns)
-small_df = total_df[total_df['ENGLISH_DAY_TYPE']==option4].drop(columns = ['PFPI_MINUTES'])
-
-
-X_processed = pipe.transform(small_df)
+# total_df = pd.read_csv('/home/yvngjoey/code/MathmoBen/TrainDelays/Notebooks/clean_data_final.csv')
+# #st.write(total_df.columns)
+# small_df = total_df[total_df['ENGLISH_DAY_TYPE']==option4].drop(columns = ['PFPI_MINUTES'])
 
 
-if st.button('Click to predict'):
-    st.text(model.predict(X_processed))
+# X_processed = pipe.transform(small_df)
+
+
+# if st.button('Click to predict'):
+#     st.text(model.predict(X_processed))
 
 
 #Sst.dataframe(small_df)
 
 
-# data = {'TRAIN_SERVICE_CODE_AFFECTED' : [22215003, 22218000, 25234001, 21235001, 22214000, 25235001,21234001, \
-#                                         22216000, 22204000, 21252001, 22206000, 21921000, 21237001, 22215002],\
-#         'SERVICE_GROUP_CODE_AFFECTED' : ['EK03', 'EK99', 'EK04', 'EK01', 'EK02', 'EK05'],
+# data = {'TRAIN_SERVICE_CODE_AFFECTED' : [option7]
+#         'SERVICE_GROUP_CODE_AFFECTED' : [option6],
 #         'ENGLISH_DAY_TYPE' : ['SA', 'WD', 'SU', 'BH', 'BD'],
 #         'APP_TIMETABLE_FLAG_AFF' : ['Y', 'N'],
 #         'UNIT_CLASS_AFFECTED': [375, 317, 315, 313, 710, 378, 321],
